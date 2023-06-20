baa99356be8167677660b85c5b81fc01b9bc17ed
Evan <evanlowell01@gmail.com>
2023-06-20 18:14:14 -0400

    final commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import './App.css';
-import { useSelector, useDispatch } from 'react-redux'
-import { darkMode, lightMode } from './features/modeSlice'
-import Nav from './components/Nav'
-import ContentWrapper from './components/ContentWrapper'
-import Footer from './components/Footer'
+import { useSelector, useDispatch, connect } from 'react-redux'
+import { clearData, fetchData, incrementId, decrementId, inputId } from './features/dataSlice'
+import { useEffect } from 'react';
 
-function App() {
+function App(props) {
   const dispatch = useDispatch()
-  const mode = useSelector((state) => state.mode)
+  const data = useSelector((state) => state.data)
 
-  const toggleMode = () => {
-    mode.darkMode ? dispatch(lightMode()) : dispatch(darkMode())
+  const renderImg = () => {
+    if(data.apiData) {
+      return <img style={{'width': '100vw'}} src={data.apiData.primaryImage} alt={data.apiData.title} />
+    } else {
+      return <p>image here</p>
+    }
   }
-  
+
+  useEffect(() => {
+    dispatch(fetchData())
+  }, [props.objectId, dispatch])
+
+
   return (
-    <div style={{ backgroundColor: mode.color1, color: 'white' }} className="App">
-      <Nav />
-      <button onClick={toggleMode}>{ mode.darkMode ? 'Light Mode' : 'Dark Mode' }</button>
-      <ContentWrapper />
-      <Footer />
+    <div className="App">
+      <div>
+        <button onClick={() => dispatch(fetchData())}>Thunk!</button>
+        <button onClick={() => dispatch(clearData())}>Clear</button>
+        <button onClick={() => dispatch(incrementId())}>Next</button>
+        <button onClick={() => dispatch(decrementId())}>Back</button>
+      </div>
+      <input value={ data.objectId } onChange={(e) => {
+        dispatch(inputId(Number(e.target.value)))
+      }} />
+      <div>
+        {data.objectId}
+        {renderImg()}
+      </div>
     </div>
   );
 }
 
-export default App;
+
+const mapStateToProps = (state, ownProps) => ({ objectId: state.data.objectId })
+
+export default connect(mapStateToProps)(App);
--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
-import modeReducer from './features/modeSlice'
+import dataReducer from './features/dataSlice'
+import { logger } from './features/middleware'
 
 export const store = configureStore({
     reducer: {
-        mode: modeReducer
-    }
+        data: dataReducer
+    },
+    middleware: [ logger ]
 })
